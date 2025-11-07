d3068c6b55bde78ca1ad955899b46e422b01cb12
Nisson-CX <nison.cx@outlook.com>
2025-11-08 02:02:25 +0800

    Java调用lua脚本改造分布式锁中的unlock功能

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,9 +1,12 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
@@ -11,6 +14,13 @@ public class SimpleRedisLock implements ILock {
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
     private StringRedisTemplate stringRedisTemplate;
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     public SimpleRedisLock(String name, StringRedisTemplate stringRedisTemplate) {
         this.name = name;
@@ -28,12 +38,20 @@ public class SimpleRedisLock implements ILock {
 
     @Override
     public void unLock() {
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        if (threadId.equals(id)) {
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        } else {
-            throw new RuntimeException("不能删除其他线程的锁");
-        }
+        //调用Lua脚本
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
     }
+
+//    @Override
+//    public void unLock() {
+//        String threadId = ID_PREFIX + Thread.currentThread().getId();
+//        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+//        if (threadId.equals(id)) {
+//            stringRedisTemplate.delete(KEY_PREFIX + name);
+//        } else {
+//            throw new RuntimeException("不能删除其他线程的锁");
+//        }
+//    }
 }
